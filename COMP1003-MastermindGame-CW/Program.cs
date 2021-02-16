3581fbf588cbdf2b186515ec3086f0b6d6777e7d
LAPTOP-NG4TP1P4\mattm <mattmaton@hotmail.co.uk>
2021-02-16 16:52:30 +0000

    Inital Commit

---
+++ COMP1003-MastermindGame-CW/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace COMP1003_MastermindGame_CW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
